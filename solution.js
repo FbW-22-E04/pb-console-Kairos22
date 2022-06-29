5ba5e5dd98dc22ebc80ff759fe14b2bca8e0bb37
Kairos22 <alandiko84@gmail.com>
2022-06-29 15:03:41 +0200

    add javascript

--- solution.js
+++ solution.js
@@ -1,1 +1,18 @@
 // write your code in this file
+console.log("This is an exercise in console logging")
+console.log("This is the value.")
+
+const firstName = 'Alan';
+const lastName = 'Diko';
+
+let city = 'Berlin';
+let job = 'Student';
+let age = 38;
+
+console.log(firstName, lastName, city, job, age);
+console.log(firstName , lastName + ' is a ' + age + ' years old ' + job + ' who is living in ' + city + ' . ' ) 
+
+
+
+
+
